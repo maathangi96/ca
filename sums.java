4307cdb2ea7de7ff932326dfd26d6d78286afe27
maathangi96 <maathangi96@gmail.com>
2017-06-15 20:42:48 +0530

    sums.java

---
+++ sums.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Scanner;
+class sums
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int N=s.nextInt();
+for(int i=0;i<=N;i++)
+{
+sum=sum+i;
+}
+System.out.pritln(sum);
+}
+}
